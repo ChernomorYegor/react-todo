859ef5adf6a14a9ed4501358d74e3db9265aea72
ChernomorYegor <chernomor.yegor@gmail.com>
2020-05-01 22:52:09 +0300

    Updates

---
+++ precache-manifest.79c6d5ab5d7904aa660296270d80e80e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f664ce047495549457b8473ab179a959",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "5876082234b56db8973f",
+    "url": "/react-todo/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "eb031fa8f448ff003066",
+    "url": "/react-todo/static/css/main.329a8c64.chunk.css"
+  },
+  {
+    "revision": "5876082234b56db8973f",
+    "url": "/react-todo/static/js/2.e23b937d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.e23b937d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb031fa8f448ff003066",
+    "url": "/react-todo/static/js/main.f053e992.chunk.js"
+  },
+  {
+    "revision": "baaf63e8524762cb973a",
+    "url": "/react-todo/static/js/runtime-main.80ec170c.js"
+  }
+]);
